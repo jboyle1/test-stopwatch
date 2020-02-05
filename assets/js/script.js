c7afb563093cf432a7297507b8bed23f9faf1d31
Jed Boyle <jboyle1@pca.ac.uk>
2020-02-05 10:38:12 +0000

    Wrote out variables to target HTML elements by ID using the document object and getElementById() method (using getElementsById to be unambiguous). Variables include: h1, Start, stop, clear. I also added variables for add() function, these include milli, seconds, minutes and hours.

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var h1 = document.getElementsByTagName('h1')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    milli = 0, seconds = 0, minutes = 0, hours = 0,
+    t;
